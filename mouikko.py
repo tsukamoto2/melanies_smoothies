d5ae4b4008fc2f98ba3bf7753c32c4b8aed4702d
tsukamoto2 <tsukamoto.kokona.5433@members.co.jp>
2025-12-03 16:36:49 +0900

    Update mouikko.py

--- mouikko.py
+++ mouikko.py
@@ -44,6 +44,9 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 
 
