77c3d868b09798a4f112f55369bbf49480a30e17
tsukamoto2 <tsukamoto.kokona.5433@members.co.jp>
2025-12-03 16:42:56 +0900

    Refactor smoothie order logic and remove unused code
    Removed unnecessary code for displaying nutrition information and adjusted the smoothie order insertion logic.

--- mouikko.py
+++ mouikko.py
@@ -28,7 +28,10 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen +' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
+    
     #st.write(ingredients_string)
 
     my_insert_stmt = """insert into smoothies.public.orders (ingredients,name_on_order)
@@ -44,12 +47,7 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
-# New section to display smoothiefroot nutrition information
+
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
-
 
     
