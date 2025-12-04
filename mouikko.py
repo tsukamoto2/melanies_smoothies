7d6ff84ceb108c0e2e098c68c7485074ae4483fc
tsukamoto2 <tsukamoto.kokona.5433@members.co.jp>
2025-12-04 09:43:40 +0900

    Update mouikko.py

--- mouikko.py
+++ mouikko.py
@@ -51,7 +51,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + 'の栄養情報')
         
         # API呼び出し（各フルーツに対応するように変更）
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" +fruit_chosen)
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}')
         
         # API応答（JSON形式）をDataFrameとしてStreamlitに表示
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
